028083e8f3134f4c089c35c1f6a290961d4e2548
anon20016 <41828236+anon20016@users.noreply.github.com>
2019-07-22 17:45:23 +0400

    no message

---
+++ Tasks_3/Task3_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter N: ");
+            string[] input = Console.ReadLine().Split();            
+            if (input.Length == 1 && Int32.TryParse(input[0], out n) && n > 0)
+            {
+                solution3_1(n);
+            }
+            else
+            {
+                Console.WriteLine("Input error!");
+            }
+        }
+        
+        static void solution3_1(int n)
+        {
+            LinkedList<int> mass = new LinkedList<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                mass.AddLast(i);
+            }
+            var t = mass.First;
+            while (mass.Count > 0)
+            {
+                Console.WriteLine("Next number: " + t.Value);
+                var nx = t.Next;
+                if (t == mass.Last)
+                {
+                    nx = mass.First;
+                }
+                mass.Remove(t);
+                if (nx == mass.Last)
+                {
+                    t = mass.First;
+                }
+                else
+                {
+                    t = nx.Next;
+                }                
+            }            
+        }
+    }
+}
---
+++ Tasks_3/Task3_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine("Enter string");
+            s = Console.ReadLine();
+            solution3_2(s);
+        }
+
+        static void solution3_2(string s)
+        {
+            string[] words = s.Split(new char[] { '.', ' '});
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (var t in words)
+            {
+                if (dict.ContainsKey(t.ToLower()))
+                {
+                    dict[t.ToLower()]++;
+                }
+                else
+                {
+                    dict.Add(t.ToLower(), 1);
+                }
+            }                   
+            foreach (var i in dict)
+            {
+                Console.WriteLine($"the number of words {i.Key} : {i.Value}");
+            }
+        }
+    }
+}
