1cfb7671a5ab0ea3252a40270f5bcd67e7b90068
anon20016 <anon20016@gmail.com>
2019-07-25 18:15:12 +0400

    no message

---
+++ Tasks_3/Task3_3/CycledDynamicArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3_3
+{
+    public class CycledDynamicArray<T> : DynamicArray<T>, IEnumerable<T>, IEnumerator<T>, ICloneable where T : IComparable
+    {
+        private int index = -1;
+
+        public CycledDynamicArray(IEnumerable<T> arr) : base(arr)
+        {
+        }       
+
+        public new bool MoveNext()
+        {
+            index++;
+            if (index == Length)
+            {
+                index = 0;
+            }
+            return true;
+        }
+    }
+}
