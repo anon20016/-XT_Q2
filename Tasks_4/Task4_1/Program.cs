a4497fc3de9a5d7cf215765d7172ad665b3ec828
anon20016 <anon20016@gmail.com>
2019-07-29 16:57:05 +0400

    no message

---
+++ Tasks_4/Task4_1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Task4_1
+{
+
+    class Program
+    {
+        public delegate bool Compare<T>(T first, T second);
+
+        static bool CompareInt(int q, int w)
+        {
+            if (q > w)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool CompareString(string s, string t)
+        {
+            if (s == null)
+            {
+                return false;
+            }
+            if (t == null)
+            {
+                return true;
+            }
+
+            if (s.Length < t.Length)
+            {
+                return false;
+            }
+
+            if (s.Length > t.Length)
+            {
+                return true;
+            }
+
+            if (s.CompareTo(t) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            CustomSortDemo();
+            
+        } 
+        
+        static void CustomSortDemo()
+        {
+            string[] t = { "123", "df", "", "df1", "a", "412", "ACAS", "aasdjasndasdfasdf", "abd", "1", "412" };
+
+            CustomSort(t, CompareString);
+            for (int i = 0; i < t.Length; i++)
+            {
+                Console.Write(t[i] + " ");
+            }
+        }
+
+        private static void swap<T>(ref T fr, ref T sc)
+        {
+            T temp = fr;
+            fr = sc;
+            sc = temp;
+        }
+        public static void CustomSort<T>(T[] arr, Compare<T> cmp)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (cmp(arr[i], arr[i + 1]))
+                    {
+                        swap(ref arr[i], ref arr[i + 1]);
+                        flag = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tasks_4/Task4_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task4_5
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter string to check: ");
+            string s = Console.ReadLine();
+            if (s.isPositiveInt())
+            {
+                Console.WriteLine("It is a positive integer!");
+            }
+            else
+            {
+                Console.WriteLine("It is not a positive integer!");
+            }
+        }
+        public static bool isPositiveInt(this string s)
+        {
+            bool flag = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == 0 && s[i] == '+')
+                    continue;
+                flag &= char.IsDigit(s[i]);
+            }
+            return flag;
+        }
+    }
+}
